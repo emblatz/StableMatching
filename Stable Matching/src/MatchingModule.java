419d50ad787aefbb449ac320e882710a5a87e1c8
Emily <emilyblatz@gmail.com>
2015-03-23 14:01:37 -0400

    Dependencies, subject to change

---
+++ Stable Matching/src/MatchingModule.java
@@ -0,0 +1,10 @@
+import com.google.inject.AbstractModule;
+
+public class MatchingModule extends AbstractModule {
+	
+	@Override
+    protected void configure() {
+        
+        bind(Matching.class).to(StableMatchingImpl.class);
+    }
+}
---
+++ Stable Matching/src/StableMatchingImpl.java
@@ -0,0 +1,11 @@
+import javax.inject.Inject;
+ 
+public class StableMatchingImpl extends Matching {
+ 
+    private Matching matcher;
+    
+    @Inject
+    public void setMatcher(Matching match) {
+    	this.matcher = match;
+    }
+}
