e2d48c58f30466d331db582a957f47bc58189a7b
Emily <emilyblatz@gmail.com>
2015-04-14 11:13:11 -0400

    Matching algorithm is finished and fixed

--- src/main/java/edu/fgcu/Matching.java
+++ src/main/java/edu/fgcu/Matching.java
@@ -25,42 +25,49 @@ public class Matching {
 		if another student B only has a GPA of 3.0, no award, and is a distance 40mi and
 		if another school T that requires a minimum of 2.0 GPA, requires no awards and is also a distance of 10mi
 		*/
-		while (!students.isEmpty()) {
-			for (Student student:students) {
-				for (School school:schools) {
-					if (!matched.containsKey(school)) {
+		ListIterator<Student> iterStudent = students.listIterator();
+		while (iterStudent.hasNext()) {
+			HashMap bestMatch = new HashMap();
+			Student student = iterStudent.next();
+			System.out.println(student.getName());
+			for (School school:schools) {
+				int match=0;
+				if(school.getDistance()>=student.getDistance()) {
+					match++;
+				}
+				if (school.getAwards()==student.getAwards()) {
+					match++;
+				}
+				if (school.getGPA()<=student.getGPA()) {
+					match++;
+				}
+				bestMatch.put(match, school);
+				if (!matched.containsKey(school)) {
+					if (school.getGPA()<=student.getGPA() && school.getDistance()>=student.getDistance()) {
 						matched.put(school, student);
+						System.out.println("Student "+student.getName()+" matched to "+school.getName());
+						iterStudent.remove();
 						break;
 					}
-					else {
-						int newCount = 0;
-						int oldCount = 0;
-						Student oldStudent = (Student) matched.get(school);
-						if (school.getDistance()>=student.getDistance()) {
-							newCount++;
-						}
-						if (school.getAwards()==student.getAwards()) {
-							newCount++;
-						}
-						if (school.getGPA()<=student.getGPA() && student.getGPA() > oldStudent.getGPA()) {
-							newCount++;
-						}
-						if (school.getDistance()>=oldStudent.getDistance()) {
-							oldCount++;
-						}
-						if (school.getAwards()==oldStudent.getAwards()) {
-							oldCount++;
-						}
-						if (school.getGPA()<=oldStudent.getGPA() && student.getGPA() <= oldStudent.getGPA()) {
-							oldCount++;
-						}
-						if (oldCount < newCount) {
-							matched.remove(school);
-							matched.put(school, student);
-							students.remove(student);
-							students.add(oldStudent);
-							break;
-						}
+				}
+				else {
+					int newCount = 0;
+					int oldCount = 0;
+					Student oldStudent = (Student) matched.get(school);
+					if (school.getAwards()==student.getAwards()) {
+						newCount++;
+					}
+					if (school.getAwards()==oldStudent.getAwards()) {
+						oldCount++;
+					}
+					if (oldCount < newCount) {
+						matched.remove(school);
+						matched.put(school, student);
+						System.out.println("New student "+student.getName()+" beat "+oldStudent.getName()+" at "+school.getName());
+						iterStudent.remove();
+						iterStudent.add(oldStudent);
+						iterStudent.previous();
+						break;
 					}
 				}
 			}
