cdf5be1f1de08446ef36932ff57c89bcf1161527
Nowele <nrechka8788@eagle.fgcu.edu>
2015-04-15 18:52:26 -0400

    part 1 of new gui main layout
    only one gui window will be left now. for now, just see both forms.
    buttons and matching section to come...

--- src/main/java/edu/fgcu/GUIMain.java
+++ src/main/java/edu/fgcu/GUIMain.java
@@ -2,26 +2,47 @@ package edu.fgcu;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 
 public class GUIMain extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel contentPane2;
+	
 	private ButtonGroup accountButtons;
 	private JRadioButton rdbtnSchool;
 	private JRadioButton rdbtnStudent;
 	private JFrame error;
+	
+	private SchoolList schoolList;
+	private JList jlistSchools;
+	private StudentList studentList;
+	private JList jlistStudents;
+	
+	private JTextField schoolTextName;
+	private JTextField schoolTextGPA;
+	private JTextField schoolTextDistance;
+	private JComboBox schoolAward;
+	
+	private JTextField studentTextName;
+	private JTextField studentTextGPA;
+	private JTextField studentTextDistance;
+	private JComboBox studentAward;
 
 	/**
 	 * Launch the application.
@@ -44,12 +65,111 @@ public class GUIMain extends JFrame {
 	 */
 	public GUIMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1000, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(new GridLayout(0, 4));
 		setContentPane(contentPane);
 		
+		//column 1
+		JLabel lblEnterSchoolInformation = new JLabel("Enter School Information:");
+		lblEnterSchoolInformation.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEnterSchoolInformation.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		contentPane.add(lblEnterSchoolInformation);
+			
+		//column 2
+		JLabel label1 = new JLabel("");
+		contentPane.add(label1); //fill a space
+		
+		//column 3
+		JLabel lblEnterStudentInformation = new JLabel("Enter Student Information:");
+		lblEnterStudentInformation.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		contentPane.add(lblEnterStudentInformation);
+		
+		//column 4
+		JLabel label2 = new JLabel("");
+		contentPane.add(label2); //fill a space
+		
+		
+		//column 1
+		JLabel lblSchoolName = new JLabel("School Name:");
+		contentPane.add(lblSchoolName);
+		
+		//column 2
+		schoolTextName = new JTextField();
+		contentPane.add(schoolTextName);
+		schoolTextName.setColumns(10);
+		
+		//column 3
+		JLabel lblNewLabel = new JLabel("Student Name:");
+		contentPane.add(lblNewLabel);
+		
+		//column 4
+		studentTextName = new JTextField();
+		contentPane.add(studentTextName);
+		studentTextName.setColumns(10);
+		
+		//column 1
+		JLabel lblMinGPA = new JLabel("Minimum GPA Requirement (0-4):");
+		contentPane.add(lblMinGPA);
+		
+		//column 2
+		schoolTextGPA = new JTextField();
+		contentPane.add(schoolTextGPA);
+		schoolTextGPA.setColumns(10);
+		
+		//column 3
+		JLabel lblStudentGPA = new JLabel("Student GPA (0-4):");
+		contentPane.add(lblStudentGPA);
+		
+		//column 4
+		studentTextGPA = new JTextField();
+		contentPane.add(studentTextGPA);
+		studentTextGPA.setColumns(10);
+		
+		//column 1
+		JLabel lblMaxDistance = new JLabel("Maximum Distance in miles (0-100):");
+		contentPane.add(lblMaxDistance);
+		
+		//column 2
+		schoolTextDistance = new JTextField();
+		contentPane.add(schoolTextDistance);
+		schoolTextDistance.setColumns(10);
+		
+		//column 3
+		JLabel lblLocation = new JLabel("Preferred Distance in miles (0-100):");
+		contentPane.add(lblLocation);
+		
+		//column 4
+		studentTextDistance = new JTextField();
+		contentPane.add(studentTextDistance);
+		studentTextDistance.setColumns(10);
+		
+		//column 1
+		JLabel lblSchoolAward = new JLabel("Select Your Preferred Award:");
+		contentPane.add(lblSchoolAward);
+		
+		//column 2
+		schoolAward = new JComboBox();
+		schoolAward.addItem("None");
+		schoolAward.addItem("Scholarship");
+		schoolAward.addItem("Honors Award");
+		schoolAward.addItem("Other");
+		contentPane.add(schoolAward);
+		
+		//column 3
+		JLabel lblStudentAward = new JLabel("Select Your Award:");
+		contentPane.add(lblStudentAward);
+		
+		//column 4
+		studentAward = new JComboBox();
+		studentAward.addItem("None");
+		studentAward.addItem("Scholarship");
+		studentAward.addItem("Honors Award");
+		studentAward.addItem("Other");
+		contentPane.add(studentAward);
+		
+		/*
 		JLabel lblChooseOne = new JLabel("Choose One:");
 		lblChooseOne.setHorizontalAlignment(SwingConstants.CENTER);
 		lblChooseOne.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -99,6 +219,6 @@ public class GUIMain extends JFrame {
     			    "One option must be selected.",
     			    "Selection error",
     			    JOptionPane.ERROR_MESSAGE);
-    	}
+    	}*/
     }
 }
