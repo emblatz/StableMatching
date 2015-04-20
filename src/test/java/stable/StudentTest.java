a82b50b119475860abf74ff8a0cb97ec6dac9be7
Nowele <nrechka8788@eagle.fgcu.edu>
2015-04-20 13:33:30 -0400

    updates to studentTest
    added more test methods

--- src/test/java/stable/StudentTest.java
+++ src/test/java/stable/StudentTest.java
@@ -8,11 +8,35 @@ import stable.Student;
 
 public class StudentTest {
 	
+	
+	
+	@Test
+	public void testStudentConstructor(){
+				
+		String name = "Jane Doe";
+		double gpa = 4.0;
+		int distance = 30;
+		String awards = "None";
+		
+		Student student = new Student(name, gpa, distance, awards);
+		
+		assertEquals(student.getName(), name);
+		assertEquals(student.getGPA(), gpa, 0);
+		assertEquals(student.getDistance(), distance);
+		assertEquals(student.getAwards(), awards);
+	}
+	
 	@Test
 	public void testSetName(){
 		Student student = new Student();
 		student.setName("John Smith");
 		assertEquals(student.getName(), "John Smith");
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testSetNameFail(){
+		Student student = new Student();
+		student.setName(null);
 	}
 	
 	@Test
@@ -24,11 +48,23 @@ public class StudentTest {
 		assertEquals(actualGPA, expectedGPA);
 	}
 	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testSetGPAFail() {
+		Student student = new Student();
+		student.setGPA(30.0);
+	}
+	
 	@Test
 	public void testSetDistance() {
 		Student student = new Student();
 		student.setDistance(50);
 		assertEquals(student.getDistance(), 50);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testSetDistanceFail() {
+		Student student = new Student();
+		student.setDistance(300);
 	}
 	
 	@Test
