6c1ce14dde7328e3bca54d5c0d8c8bf4bf33d9a7
Nowele <nrechka8788@eagle.fgcu.edu>
2015-04-20 13:44:44 -0400

    updates to studentTest and schoolTest

--- src/test/java/stable/SchoolTest.java
+++ src/test/java/stable/SchoolTest.java
@@ -7,12 +7,34 @@ import org.junit.Test;
 import stable.School;
 
 public class SchoolTest {
+	
+	@Test
+	public void testSchoolConstructor(){
+				
+		String name = "School University";
+		double gpa = 1.5;
+		int distance = 100;
+		String awards = "None";
+		
+		School school = new School(name, gpa, distance, awards);
+		
+		assertEquals(school.getName(), name);
+		assertEquals(school.getGPA(), gpa, 0);
+		assertEquals(school.getDistance(), distance);
+		assertEquals(school.getAwards(), awards);
+	}
 
 	@Test
 	public void testSetName(){
 		School school = new School();
 		school.setName("Florida Gulf Coast University");
 		assertEquals(school.getName(), "Florida Gulf Coast University");
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testSetNameFaiil(){
+		School school = new School();
+		school.setName("");
 	}
 	
 	// Floating point numbers are causing a problem.
@@ -26,11 +48,23 @@ public class SchoolTest {
 		assertEquals(actualGPA, expectedGPA);
 	}
 	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testSetGPAFail() {
+		School school = new School();
+		school.setGPA(-2.5);
+	}
+	
 	@Test
 	public void testSetDistance() {
 		School school = new School();
 		school.setDistance(100);
 		assertEquals(school.getDistance(), 100);
+	}
+	
+	@Test
+	public void testSetDistanceFail() {
+		School school = new School();
+		school.setDistance(1000);
 	}
 	
 	@Test
